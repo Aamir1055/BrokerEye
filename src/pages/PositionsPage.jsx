96e3f980f7703d5cbb31c7f61f2761441bce4b82
Aamir1055 <shaikhaamir056@gmail.com>
2025-12-12 21:42:48 +0400

    Client Net: add fixed height to table container to show vertical scrollbar

--- src/pages/PositionsPage.jsx
+++ src/pages/PositionsPage.jsx
@@ -3062,7 +3062,7 @@ const PositionsPage = () => {
                       </button>
                     </div>
                 </div>
-                <div className="overflow-x-auto overflow-y-auto flex-1">
+                <div className="overflow-x-auto overflow-y-auto flex-1 h-[420px]">
                   {clientNetPositionsData.length === 0 && !isInitialPositionsLoading ? (
                     <div className="text-center py-12">
                       <svg className="w-12 h-12 mx-auto text-gray-400 mb-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
