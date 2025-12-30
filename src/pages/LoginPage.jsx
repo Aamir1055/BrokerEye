74326b6a2ac0d5ed5abe8dbb783d8eff6ee00db7
Aamir1055 <shaikhaamir056@gmail.com>
2025-12-30 17:52:04 +0400

    Desktop login: apply ellipse clipping to create tapering circle effect

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -196,43 +196,43 @@ const LoginPage = () => {
       <div className="hidden lg:block pointer-events-none absolute inset-0 z-0">
         {/* Ellipse 49 */}
          <div 
-           className="absolute rounded-full" 
+           className="absolute" 
            style={{
             width: '1600px',
             height: '1600px',
             right: '-800px',
             top: 'calc(54% - 800px)',
             background: '#4471D6',
-            WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 60%, rgba(0,0,0,0.5) 85%, rgba(0,0,0,0) 100%)',
-            maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 60%, rgba(0,0,0,0.5) 85%, rgba(0,0,0,0) 100%)'
+            borderRadius: '50%',
+            clipPath: 'ellipse(50% 50% at 100% 35%)',
            }}
          />
         
         {/* Ellipse 51 */}
          <div 
-           className="absolute rounded-full" 
+           className="absolute" 
            style={{
             width: '1550px',
             height: '1550px',
             right: '-775px',
             top: 'calc(58% - 775px)',
             background: '#3B65C5',
-            WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 55%, rgba(0,0,0,0.5) 80%, rgba(0,0,0,0) 100%)',
-            maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 55%, rgba(0,0,0,0.5) 80%, rgba(0,0,0,0) 100%)'
+            borderRadius: '50%',
+            clipPath: 'ellipse(50% 50% at 100% 32%)',
            }}
          />
         
         {/* Ellipse 50 */}
          <div 
-           className="absolute rounded-full" 
+           className="absolute" 
            style={{
             width: '1500px',
             height: '1500px',
             right: '-750px',
             top: 'calc(62% - 750px)',
             background: '#1641A2',
-            WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0.5) 75%, rgba(0,0,0,0) 100%)',
-            maskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0.5) 75%, rgba(0,0,0,0) 100%)'
+            borderRadius: '50%',
+            clipPath: 'ellipse(50% 50% at 100% 30%)',
            }}
          />
         
