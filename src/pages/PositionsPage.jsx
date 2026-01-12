3488cd4651f18965b3152949807ea4a655fd792f
Shaik <shaik@brokerseye.com>
2026-01-12 16:01:24 +0400

    Remove bold styling from VALUE input fields in condition layout - Added font-normal class to Number Filters VALUE input - Added font-normal class to Number Filters AND input (between condition) - Added font-normal class to Text Filters VALUE input

--- src/pages/PositionsPage.jsx
+++ src/pages/PositionsPage.jsx
@@ -1938,7 +1938,7 @@ const PositionsPage = () => {
                                   setShowCustomFilterModal(false)
                                 }
                               }}
-                              className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white"
+                              className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white font-normal"
                             />
                           </div>
 
@@ -1961,7 +1961,7 @@ const PositionsPage = () => {
                                     setShowCustomFilterModal(false)
                                   }
                                 }}
-                                className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white"
+                                className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white font-normal"
                               />
                             </div>
                           )}
@@ -2093,7 +2093,7 @@ const PositionsPage = () => {
                                     setShowCustomFilterModal(false)
                                   }
                                 }}
-                                className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white"
+                                className="w-full px-2 py-1.5 text-xs border border-gray-300 rounded focus:outline-none focus:border-blue-500 text-gray-900 bg-white font-normal"
                               />
                             </div>
 
