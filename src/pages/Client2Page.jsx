df08e20dbf78eeba70cf2c7c135067fceb269c69
Your Name <your-email@example.com>
2026-01-17 17:58:48 +0530

    Fixed dashboard to use totalsPercent when percentage mode is active

--- src/pages/Client2Page.jsx
+++ src/pages/Client2Page.jsx
@@ -4091,10 +4091,15 @@ const Client2Page = () => {
             </div>
           </div>
 
-          {/* New Dashboard Section - Always visible regardless of percentage mode */}
-          {totals && Object.keys(totals).length > 0 && (
+          {/* New Dashboard Section - Shows with either normal or percentage totals */}
+          {((totals && Object.keys(totals).length > 0) || (totalsPercent && Object.keys(totalsPercent).length > 0)) && (
             <div className="mb-6">
-              <ClientDashboard totals={totals} clients={clients} totalClients={totalClients} rebateTotals={rebateTotals} />
+              <ClientDashboard 
+                totals={cardFilterPercentMode ? totalsPercent : totals} 
+                clients={clients} 
+                totalClients={totalClients} 
+                rebateTotals={rebateTotals} 
+              />
             </div>
           )}
 
