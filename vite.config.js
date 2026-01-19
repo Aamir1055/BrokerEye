498409b8486196442ccf9383564e2a6da866598b
Shaik <shaik@brokerseye.com>
2026-01-19 14:47:47 +0400

    Set broker branch to root path (remove amari-capital-new)

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // Set base path for amari-capital-new deployment
-  base: '/amari-capital-new/',
+  // Broker branch runs at root
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
